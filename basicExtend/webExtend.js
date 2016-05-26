f5e66ea912839f2b4ab567c793528c72eef49172
Sumer <livialiu1234@outlook.com>
2016-05-26 15:31:06 +0800

    增加url解析方法
    使用方法
    var myURL = parseURL('http://abc.com:8080/dir/index.html?id=255&m=hello#top');
     
    myURL.file;     // = 'index.html'
    myURL.hash;     // = 'top'
    myURL.host;     // = 'abc.com'
    myURL.query;    // = '?id=255&m=hello'
    myURL.params;   // = Object = { id: 255, m: hello }
    myURL.path;     // = '/dir/index.html'
    myURL.segments; // = Array = ['dir', 'index.html']
    myURL.port;     // = '8080'
    myURL.protocol; // = 'http'
    myURL.source;   // = 'http://abc.com:8080/dir/index.html?id=255&m=hello#top'

--- basicExtend/webExtend.js
+++ basicExtend/webExtend.js
@@ -1,9 +1,38 @@
 var Sumer = Sumer || {};
 Sumer.cookie = {
-  setCookie :function (c_name,value,expiredays){
+	setCookie :function (c_name,value,expiredays){
 		var exdate=new Date()
 		exdate.setDate(exdate.getDate()+expiredays)
 		document.cookie=c_name+ "=" +escape(value)+
 		((expiredays==null) ? "" : ";expires="+exdate.toGMTString())+";path=/"
-	},
+	}
 };
+Sumer.URL = {
+	parseURL:function(url) {
+    		var a =  document.createElement('a');
+    		a.href = url;
+    		return {
+        		source: url,
+        		protocol: a.protocol.replace(':',''),
+        		host: a.hostname,
+        		port: a.port,
+        		query: a.search,
+        		params: (function(){
+            			var ret = {},
+                		seg = a.search.replace(/^?/,'').split('&'),
+                		len = seg.length, i = 0, s;
+            			for (;i<len;i++) {
+                			if (!seg[i]) { continue; }
+                			s = seg[i].split('=');
+                			ret[s[0]] = s[1];
+            			}
+            			return ret;
+        		})(),
+        		file: (a.pathname.match(//([^/?#]+)$/i) || [,''])[1],
+        		hash: a.hash.replace('#',''),
+        		path: a.pathname.replace(/^([^/])/,'/$1'),
+        		relative: (a.href.match(/tps?://[^/]+(.+)/) || [,''])[1],
+        		segments: a.pathname.replace(/^//,'').split('/')
+    		};
+	}
+}
